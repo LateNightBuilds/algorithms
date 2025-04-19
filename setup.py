3cb57c28dfd639323566fb3c05d1e1618007a6cc
Yovel Ben Azaria <yovelbenazaria@gmail.com>
2025-04-19 15:54:57 +0200

    Added required packages to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,16 @@ setup(
     include_package_data=True,
     install_requires=[
         "flask",
-        "networkx"
+        "setuptools~=68.2.0",
+        "networkx~=3.4.2",
+        "numpy~=2.2.4",
+        "scikit-learn~=1.6.1",
+        "scipy~=1.15.2",
+        "librosa~=0.11.0",
+        "matplotlib~=3.10.1",
+        "pandas~=2.2.3",
+        "PyWavelets~=1.8.0",
+        "pillow~=11.2.1"
     ],
     author="Your Name",
     author_email="your.email@example.com",
